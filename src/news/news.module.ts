c8b04a63aa7dc470d991b36c8467acb76af4bd09
Vladimir <Vladimir.Stognij@gmail.com>
2023-02-16 16:13:48 +0200

    after rebase

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NewsService } from './news.service';
+import { UserService } from 'src/user/user.service';
 import { NewsController } from './news.controller';
 import { New, NewSchema } from 'src/db-schema/new.schema';
 
 @Module({
-  providers: [NewsService],
+  providers: [NewsService, UserService],
   controllers: [NewsController],
-  imports: [
-    MongooseModule.forFeature([
-      { name: New.name, schema: NewSchema }
-    ])
-  ],
+  imports: [MongooseModule.forFeature([{ name: New.name, schema: NewSchema }])],
 })
 export class NewsModule {}
