30a6ed4284d9febbc38a17b6a5227c62195e63a8
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-02-18 14:00:06 +0200

    fix

--- src/notice/dto/uploaded-files.dto.ts
+++ src/notice/dto/uploaded-files.dto.ts
@@ -2,16 +2,16 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsFile, IsFileSize } from 'src/decorators';
 
 export class UploadedFilesDto {
-  // @ApiProperty({
-  //   type: 'array',
-  //   items: {
-  //     type: 'string',
-  //     format: 'binary',
-  //     example: "['https://team-project-react-node.s3.amazonaws.com/image/3f0cd17f-d25f-491a-8cce-00bf993edc5f.jpg']",
-  //   },
+  @ApiProperty({
+    type: 'array',
+    items: {
+      type: 'string',
+      format: 'binary',
+      example: "['https://team-project-react-node.s3.amazonaws.com/image/3f0cd17f-d25f-491a-8cce-00bf993edc5f.jpg']",
+    },
 
-  //   required: false,
-  // })
+    required: false,
+  })
   @IsFile()
   @IsFileSize()
   readonly picture: string[];
