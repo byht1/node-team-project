c9adb3edd84a5fbf74334e7804732457cbc678bc
Veronika Kravchenko <aniregma@gmail.com>
2023-02-19 00:22:08 +0200

    Update pets.schema.ts

--- src/db-schema/pets.schema.ts
+++ src/db-schema/pets.schema.ts
@@ -1,27 +1,35 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
 import mongoose, { Document } from "mongoose";
 
 export type PetDocument = Pet & Document;
 
 @Schema()
 export class Pet {
+    @ApiProperty({example: '63f139e997fc630d8da1ff68'})
     _id: mongoose.Schema.Types.ObjectId;
 
+    @ApiProperty({example: 'Jack', description: 'Pet name'})
     @Prop({ required: [true, 'name is required'] })
     name: string;
 
+    @ApiProperty({example: '22.04.2018'})
     @Prop()
     birth: string;
-
+    
+    @ApiProperty({example: 'Persian cat'})
     @Prop({ required: [true, 'breed is required'] })
     breed: string;
 
-    @Prop({ required: [true, 'photo is required'] })
+    @ApiProperty({example: 'Image url'})
+    @Prop()
     image: string;
 
+    @ApiProperty({example: 'Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur  Lorem ipsum dolor sit amet, consectetur Lorem'})
     @Prop({ required: [true, 'comments are required'] })
     comments: string;
     
+    // @ApiProperty({example: '63f139e997fc630d8da1ff68'})
     // @Prop({ required: true })
     // owner: string;
 }
