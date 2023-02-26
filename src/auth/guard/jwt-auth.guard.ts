4e8e5733cfc129b7df1f5641a8f9fb49425c30d9
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-02-26 22:14:20 +0200

    fix

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -3,7 +3,6 @@ import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Users, UsersDocument } from 'src/db-schema/user.schema';
-import { validate } from 'class-validator';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
