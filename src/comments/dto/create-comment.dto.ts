8184ba0763ee5baf2a19977227a074b933df9c14
Veronika Kravchenko <aniregma@gmail.com>
2023-04-18 19:36:25 +0300

    Added regex pattern to prevent empty input values

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,7 +1,8 @@
-import { IsString, Length } from 'class-validator';
+import { IsString, Length, Matches } from 'class-validator';
 
 export class CreateCommentDto {
     @IsString({ message: 'Text should be a string' })
+    @Matches(/^\s*\S/, { message: 'The field shouldn\'t be empty' })
     @Length(2, 200, {message: 'Text should be from 2 to 200 symbols'})
     readonly text: string;
 }
